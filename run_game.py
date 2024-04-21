33383f972e956ee9c74820c54028a3f107aec523
Pawel Bednarz <pbedn@users.noreply.github.com>
2024-04-21 22:26:36 +0200

    Change background color from black

--- run_game.py
+++ run_game.py
@@ -9,4 +9,5 @@ from game.director import GameDirector
 if __name__ == '__main__':
     window = GameDirector(width=1024, height=600, caption="Set Game", resizable=False)
     pyglet.clock.schedule_interval(window.update, window.frame_rate)
+    pyglet.gl.glClearColor(0.173, 0.243, 0.314, 1.0)
     pyglet.app.run()
